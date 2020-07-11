f9b5a33514162bfed2fca33d01cb30f41d0952aa
jiro4989 <jiroron666@gmail.com>
2020-07-11 09:38:35 +0000

    move variable definitions

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,6 +12,9 @@ set -eux
   --description:"$INPUT_DESC" \
   --license:"$INPUT_LICENSE"
 
+readonly RPMBUILD_DIR="$HOME/rpmbuild"
+readonly RPMBUILD_SOURCE_DIR="$RPMBUILD_DIR/SOURCES"
+readonly RPMBUILD_SPEC_DIR="$RPMBUILD_DIR/SPECS"
 (
   readonly WORKDIR="/tmp/work"
   mkdir "$WORKDIR"
@@ -20,9 +23,6 @@ set -eux
   cd "$WORKDIR"
   tar czf tmp.tar.gz "$INPUT_PACKAGE/"
 
-  readonly RPMBUILD_DIR="$HOME/rpmbuild"
-  readonly RPMBUILD_SOURCE_DIR="$RPMBUILD_DIR/SOURCES"
-  readonly RPMBUILD_SPEC_DIR="$RPMBUILD_DIR/SPECS"
   mkdir -p "$RPMBUILD_SOURCE_DIR"
   mkdir -p "$RPMBUILD_SPEC_DIR"
   mv tmp.tar.gz "$RPMBUILD_SOURCE_DIR"
